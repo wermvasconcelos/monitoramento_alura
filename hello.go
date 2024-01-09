b1c2d5c3131d5306ecc82083a500dae80d6dbfe5
Wermenson Vasconcelos <wermenson.vasconcelo@gmail.com>
2024-01-09 19:40:25 -0300

    first commit

---
+++ hello.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var nome string = "Wermenson"
+	versao := 1.1
+
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+
+	fmt.Println("O comando escolhido foi ", comando)
+}
